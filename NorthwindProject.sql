0de27cd8856f813e9c59b5adc22063662c7bb6ef
Ahmet Çetinkaya <ahmetcetinkaya4@outlook.com>
2021-03-04 16:44:32 +0300

    Add Sql files

---
+++ NorthwindProject.sql
@@ -0,0 +1,26 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [Id]           INT           IDENTITY (1, 1) NOT NULL,
+    [FirstName]    NVARCHAR (50) NOT NULL,
+    [LastName]     NVARCHAR (50) NOT NULL,
+    [Email]        NVARCHAR (50) NOT NULL,
+    [PasswordHash] VARBINARY(500)  NOT NULL,
+    [PasswordSalt] VARBINARY(500)  NOT NULL,
+    [Status]       BIT           NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+CREATE TABLE [dbo].[UserOperationClaims] (
+    [Id]               INT IDENTITY (1, 1) NOT NULL,
+    [UserId]           INT NOT NULL,
+    [OperationClaimId] INT NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_UserOperationClaims_Users] FOREIGN KEY ([UserId]) REFERENCES [dbo].[Users] ([Id]),
+    CONSTRAINT [FK_UserOperationClaims_OperationClaims] FOREIGN KEY ([OperationClaimId]) REFERENCES [dbo].[OperationClaims] ([Id])
+);
+
+CREATE TABLE [dbo].[OperationClaims] (
+    [Id]   INT           IDENTITY (1, 1) NOT NULL,
+    [Name] VARCHAR (250) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
