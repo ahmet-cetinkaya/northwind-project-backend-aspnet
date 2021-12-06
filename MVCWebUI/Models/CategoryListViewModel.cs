3bef90b5c198fdda3bbb74d6c9a840f078ba0b06
Ahmet Çetinkaya <mail@ahmetcetinkaya.info>
2021-12-06 21:48:05 +0300

    ✨ (MVCWebUI): Category

---
+++ MVCWebUI/Models/CategoryListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Entities.Concrete;
+
+namespace MVCWebUI.Models
+{
+    public class CategoryListViewModel
+    {
+        public List<Category> Categories { get; set; }
+        public int ActiveCategoryId { get; set; }
+    }
+}
---
+++ MVCWebUI/ViewComponents/CategoryListViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using MVCWebUI.Models;
+
+namespace MVCWebUI.ViewComponents
+{
+    public class CategoryListViewComponent : ViewComponent
+    {
+        private ICategoryService _categoryService;
+
+        public CategoryListViewComponent(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public ViewViewComponentResult Invoke()
+        {
+            var model = new CategoryListViewModel
+            {
+                Categories = _categoryService.GetAll().Data,
+                ActiveCategoryId = Convert.ToInt32(HttpContext.Request.Query["category"])
+            };
+            return View(model);
+        }
+    }
+}
